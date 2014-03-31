a8622c996fb3267044ad8ad7574aae4a31ede9ac
Bhathiya90 <hsbathiya@gmail.com>
2014-03-31 11:31:22 +0530

    Azureblob live test bug fix

--- providers/azureblob/src/test/java/org/jclouds/azureblob/AzureBlobClientLiveTest.java
+++ providers/azureblob/src/test/java/org/jclouds/azureblob/AzureBlobClientLiveTest.java
@@ -87,7 +87,9 @@ public class AzureBlobClientLiveTest extends BaseBlobStoreIntegrationTest {
    public void testCreateContainer() throws Exception {
       boolean created = false;
       while (!created) {
-         privateContainer = prefix + new SecureRandom().nextInt();
+
+         privateContainer = (CONTAINER_PREFIX + new SecureRandom().nextInt());
+
          try {
             created = getApi().createContainer(privateContainer, withMetadata(ImmutableMultimap.of("foo", "bar")));
          } catch (UndeclaredThrowableException e) {
@@ -353,7 +355,9 @@ public class AzureBlobClientLiveTest extends BaseBlobStoreIntegrationTest {
 
    @Test(timeOut = 5 * 60 * 1000)
    public void testBlockOperations() throws Exception {
-      String blockContainer = prefix + new SecureRandom().nextInt();
+
+      String blockContainer = (CONTAINER_PREFIX + new SecureRandom().nextInt());
+
       String blockBlob = "myblockblob-" + new SecureRandom().nextInt();
       String A = "A";
       String B = "B";
